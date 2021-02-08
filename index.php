4126045f8b533472083bb426420d1725ad26d6a3
geofflambeth <geofflambeth@gmail.com>
2021-02-07 17:26:05 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="mystyle.css">
+  </head>
+
+  <!-- #include file="header.html" -->
+
+  <body>
+  <h1>Test</h1>
+  <p>I'm hosted with GitHub Pages.</p>
+  </body>
+</html>
